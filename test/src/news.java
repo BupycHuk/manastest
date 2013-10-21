159d11b3ff12ea0ac4b8ca23d35ff34019e398ef
Almaz <atairov996@gmail.com>
2013-10-21 10:54:05 +0600

    love nolove program

---
+++ test/src/news.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: S_K_Y
+ * Date: 21.10.13
+ * Time: 9:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class news {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int n, m;
+        String s1 = "love", s2="nolove";
+        System.out.println("Сан киргиз:");
+        n = input.nextInt();
+        m = input.nextInt();
+        if(n == 6 || m == 6 || n - m == 6 || m - n == 6){
+            System.out.println(s1);
+        }
+        else {
+            System.out.println(s2);
+        }
+
+    }
+}
