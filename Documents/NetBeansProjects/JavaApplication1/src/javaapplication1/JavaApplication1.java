fdb61583a6dc821d34bdb6dc04884def8d852a0c
Imanali <iman.mamadiyev@gmail.com>
2013-10-27 18:43:26 +0600

    fixed #23

--- Documents/NetBeansProjects/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ Documents/NetBeansProjects/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -3,27 +3,25 @@ package javaapplication1;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-/**
- *
- * @author Imanali
- */
 public class JavaApplication1 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Scanner input =new Scanner(System.in);
-        int a,b,c;
-        a=input.nextInt();
-        b=input.nextInt();
-        c=input.nextInt();
-        if(a<b+c){
-            System.out.print("NO");
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        Byte[] b = new Byte[n];
+        for(int i=0;i<n;i++){
+            b[i]=input.nextByte();
         }
-        else{
-            System.out.print("YES");
-        }
+        Arrays.sort(b);
+
+        List<Byte> asList = Arrays.asList(b);
+        List<Byte> l = new LinkedList<Byte>(asList);
+        l.remove(0);
+        Byte[] res = (Byte[]) l.toArray(new Byte[l.size()]);
+        System.out.println("result array "+
+        Arrays.toString(res));
     }
+
 }
